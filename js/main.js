cb9cd8a818a79deed79f98e589db4dd773f0be1b
jacob <jwh315@cox.net>
2015-01-08 21:32:55 -0700

    switch to Phaser.Auto, webgl uses less resources when it is available

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(900, 650, Phaser.CANVAS, '');
+var game = new Phaser.Game(1000, 650, Phaser.AUTO, '');
 
 game.state.add('Boot', PrincessVsPirates.Boot);
 game.state.add('Preload', PrincessVsPirates.Preload);
